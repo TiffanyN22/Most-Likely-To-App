42e43b6251782bd5f062681ab3aea1e898b8f229
TiffanyN22 <tnguyen22@ndsj.org>
2022-12-08 23:54:29 -0800

    Working code to display prompts and change background color

---
+++ MostLikelyTo/MostLikelyToApp.swift
@@ -0,0 +1,17 @@
+//
+//  MostLikelyToApp.swift
+//  MostLikelyTo
+//
+//  Created by Tiffany Nguyen on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MostLikelyToApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
